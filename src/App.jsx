d68409434e328fd86a3dfccfbbc3177980586c79
Abinash Rai <abinashbantawa1905@gmail.com>
2023-09-27 14:23:24 +0545

    added background image and installed tsparticles package

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Design from "./components/Design";
+
+function App() {
+  return (
+    <div className="">
+      <img src="/public/bannerBg.jpg" alt="" />
+      <Design />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Design.jsx
@@ -0,0 +1,9 @@
+// import { useCallback } from "react";
+// import Particles from "react-particles";
+// import { loadFull } from "tsparticles";
+
+const Design = () => {
+  return <div>DEsign</div>;
+};
+
+export default Design;
